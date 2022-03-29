881c3c0156e3ec0c46bdee9213fc807cc6dfa54d
Полина <Shumilina246@gmail.com>
2022-03-29 15:11:54 +0300

    fix: styles for grid

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import { GiphyFetch } from "@giphy/js-fetch-api";
 import { useState } from "react";
-import { GifList } from "./components/GifList/GifList";
+import { CustomGrid } from "./components/CustomGrid/CustomGrid";
 import { Input } from "./components/Input/Input";
 
 function App() {
   const [value, setValue] = useState("");
-  const [result, setResult] = useState([]);
-
-  const giphy = new GiphyFetch(process.env.REACT_APP_GIPHY_KEY);
-
-  const callGiphy = async () => {
-    const result = await giphy.search("cat", {
-      sort: "relevant",
-      lang: "es",
-      limit: 5,
-      type: "gifs",
-    });
-    setResult(result.data);
-  };
 
   const listenInput = (event) => {
     setValue(event.target.value);
   };
-
-  callGiphy();
 
   return (
     <div className="App">
       <section className="container">
         <div className="gif__container">
           <div className="gif__content">
-            <GifList result={result} />
+            <CustomGrid temp={"cat"} />
           </div>
         </div>
         <div className="input__container">
---
+++ src/components/CustomGrid/CustomGrid.js
@@ -0,0 +1,41 @@
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { Grid } from "@giphy/react-components";
+import React, { useState } from "react";
+import ResizeObserver from "react-resize-observer";
+
+export const CustomGrid = ({ temp }) => {
+  const [width, setWidth] = useState(0);
+  console.log(width);
+
+  const giphy = new GiphyFetch(process.env.REACT_APP_GIPHY_KEY);
+
+  const callGiphy = async (offset) => {
+    const res = await giphy.search(`${temp}`, {
+      offset,
+      sort: "relevant",
+      lang: "es",
+      limit: 10,
+      type: "gifs",
+    });
+    return res;
+  };
+
+  return (
+    <>
+      <Grid
+        width={width}
+        columns={3}
+        gutter={6}
+        fetchGifs={callGiphy}
+        noResultsMessage={"Please, try again"}
+        // onGifClick={onGifClick}
+      />
+
+      <ResizeObserver
+        onResize={({ width }) => {
+          setWidth(width-24);
+        }}
+      />
+    </>
+  );
+};
